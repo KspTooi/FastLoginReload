e007ec379ea77926efe7b3b557d26f4cca0b9a52
90Neko <ksptooi@protonmail.ch>
2020-10-27 16:58:14 +0800

    反射相关

--- FastLoginR-PROCESSOR/src/main/java/com/ksptooi/flr/starter/FastLoginR.java
+++ FastLoginR-PROCESSOR/src/main/java/com/ksptooi/flr/starter/FastLoginR.java
@@ -2,6 +2,7 @@ package com.ksptooi.flr.starter;
 
 import com.ksptooi.flr.service.player.PlayerService;
 import com.ksptooi.flr.starter.spring.ContextR;
+import com.ksptooi.flr.starter.spring.JarUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.util.logging.Logger;
@@ -16,12 +17,14 @@ public class FastLoginR extends JavaPlugin {
         Logger logger = Bukkit.getLogger();
         logger.info("[FastLoginR]版本:"+currentVersion);
         logger.info("[FastLoginR]尝试注入Services:");
+        logger.info("[FastLoginR]");
 
 
-        PlayerService playerService = ContextR.get(PlayerService.class);
+
+/*        PlayerService playerService = ContextR.get(PlayerService.class);
 
         logger.info("[FastLoginR]"+playerService);
-        playerService.playerRegister(null);
+        playerService.playerRegister(null);*/
 
     }
 

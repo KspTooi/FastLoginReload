ac1eff57800ca48c444d8a90f2075429f305cc11
90Neko <ksptooi@protonmail.ch>
2020-10-30 01:36:09 +0800

    数据相关接口 V1.3F.56

--- FastLoginR-PROCESSOR/src/main/java/com/ksptooi/flr/starter/FastLoginR.java
+++ FastLoginR-PROCESSOR/src/main/java/com/ksptooi/flr/starter/FastLoginR.java
@@ -15,7 +15,7 @@ import java.util.logging.Logger;
 
 public class FastLoginR extends JavaPlugin {
 
-    public static final String currentVersion = "1.3F.43";
+    public static final String currentVersion = "1.3F.56";
 
     @Override
     public void onEnable() {

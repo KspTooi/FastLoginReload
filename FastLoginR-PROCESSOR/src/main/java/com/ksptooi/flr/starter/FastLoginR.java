8c3df2e809664ad9477cc187dec089b91151a261
90Neko <ksptooi@protonmail.ch>
2020-10-30 02:54:18 +0800

    新增批量注册测试类 V1.3F.80

--- FastLoginR-PROCESSOR/src/main/java/com/ksptooi/flr/starter/FastLoginR.java
+++ FastLoginR-PROCESSOR/src/main/java/com/ksptooi/flr/starter/FastLoginR.java
@@ -15,7 +15,7 @@ import java.util.logging.Logger;
 
 public class FastLoginR extends JavaPlugin {
 
-    public static final String currentVersion = "1.3F.62";
+    public static final String currentVersion = "1.3F.80";
 
     @Override
     public void onEnable() {
--- FastLoginR-PROCESSOR/src/test/java/com/ksptooi/flr/service/test/PlayerServiceTest.java
+++ FastLoginR-PROCESSOR/src/test/java/com/ksptooi/flr/service/test/PlayerServiceTest.java
@@ -5,25 +5,52 @@ import com.google.inject.Injector;
 import com.ksptooi.flr.entity.player.FLRPlayer;
 import com.ksptooi.flr.module.export.ProcModule;
 import com.ksptooi.flr.service.player.PlayerService;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.UUID;
 
 /**
  * 测试玩家服务类
  */
 public class PlayerServiceTest {
 
+    PlayerService service = null;
+
+    @Before
+    public void before(){
+        System.out.println("初始化Service");
+        this.service = ProcModule.getInject().getInstance(PlayerService.class);
+    }
+
 
     //注册玩家
     @Test
     public void regPlayer(){
 
-        PlayerService playerService = ProcModule.getInject().getInstance(PlayerService.class);
-
         FLRPlayer player = new FLRPlayer();
         player.setAccount("KspTooi");
         player.setPassword("123456");
+        service.playerRegister(player);
 
-        playerService.playerRegister(player);
+    }
+
+    //批量注册玩家
+    @Test
+    public void regPlayerBatch(){
+
+        for(int i=0;i<2000;i++){
+
+            String uuid = UUID.randomUUID().toString();
+
+            FLRPlayer player = new FLRPlayer();
+            player.setAccount(uuid);
+            player.setPassword(uuid);
+
+            service.playerRegister(player);
+            System.out.println("已注册玩家:"+uuid);
+        }
 
     }
 

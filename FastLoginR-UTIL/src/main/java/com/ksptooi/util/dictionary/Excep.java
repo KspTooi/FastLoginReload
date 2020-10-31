3d8c4708ddc9b0467c38a212e3cc1de23d8b40f5
90Neko <ksptooi@protonmail.ch>
2020-11-01 01:45:20 +0800

    错误代码枚举类

--- FastLoginR-UTIL/src/main/java/com/ksptooi/util/dictionary/Excep.java
+++ FastLoginR-UTIL/src/main/java/com/ksptooi/util/dictionary/Excep.java
@@ -6,6 +6,7 @@ public enum Excep {
     AUTH_PWD_INVALID("无效的密码",400)
     ,AUTH_ALREADY_REG("玩家已经注册",401)
     ,AUTH_ALREADY_LOG("玩家已经登录",402)
+    ,AUTH_NO_REG("玩家还没有注册!",403)
     ,FATAL_DB("数据库连接错误!",500)
 
 

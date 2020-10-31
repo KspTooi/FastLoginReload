3eb92225648eb2f4dd64904c3574756f09aa5dcb
90Neko <ksptooi@protonmail.ch>
2020-11-01 00:11:41 +0800

    新的适配器注解

--- FastLoginR-PROCESSOR/src/main/java/com/ksptooi/flr/input/command/PlayerCommandHandler.java
+++ FastLoginR-PROCESSOR/src/main/java/com/ksptooi/flr/input/command/PlayerCommandHandler.java
@@ -5,6 +5,7 @@ import com.google.inject.Inject;
 import com.ksptooi.flr.input.annotation.CommandHandler;
 import com.ksptooi.flr.input.annotation.CommandMapper;
 import com.ksptooi.flr.input.annotation.Params;
+import com.ksptooi.flr.input.annotation.PlayerOnly;
 import com.ksptooi.flr.service.player.PlayerService;
 import org.apache.ibatis.annotations.Param;
 import org.bukkit.command.Command;
@@ -16,7 +17,6 @@ public class PlayerCommandHandler {
     @Inject
     PlayerService service = null;
 
-
     /**
      * 用于玩家登录
      * @param name
@@ -25,16 +25,15 @@ public class PlayerCommandHandler {
      * @param c
      * @return 成功返回true 失败返回false
      */
+    @PlayerOnly
     @CommandMapper(value = "login",alias = {"l","log"})
     public boolean playerLogin(@Params("name")String name,
                                @Params("sender")CommandSender sender,
                                @Params("params")String[] p,
                                @Params("cmd") Command c){
 
-
         sender.sendMessage("命令名:"+name);
         sender.sendMessage("命令参数:"+p.toString());
-
 
         return true;
     }
@@ -46,6 +45,7 @@ public class PlayerCommandHandler {
      * @param p
      * @return
      */
+    @PlayerOnly
     @CommandMapper("register")
     public boolean playerRegister(@Params("sender")CommandSender s,
                                   @Params("params")String[] p){

cba371fd5e73625b78d1abc5958667318b6692c4
90Neko <ksptooi@protonmail.ch>
2020-11-01 02:13:21 +0800

    优化提示

--- FastLoginR-UTIL/src/main/java/com/ksptooi/util/dictionary/Excep.java
+++ FastLoginR-UTIL/src/main/java/com/ksptooi/util/dictionary/Excep.java
@@ -7,7 +7,7 @@ public enum Excep {
     ,AUTH_ALREADY_REG("玩家已经注册",401)
     ,AUTH_ALREADY_LOG("玩家已经登录",402)
     ,AUTH_NO_REG("玩家还没有注册!",403)
-    ,FATAL_DB("数据库连接错误!",500)
+    ,FATAL_DB("严重错误,数据库连接问题.",500)
 
 
 

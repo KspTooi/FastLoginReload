6e75c037485e9bb03d8c062e16bcfae03b2e063d
90Neko <ksptooi@protonmail.ch>
2020-11-02 00:54:48 +0800

    测试

---
+++ FastLoginR-PROCESSOR/src/main/java/com/ksptooi/flr/input/processor/TestProcessorSingle.java
@@ -0,0 +1,31 @@
+package com.ksptooi.flr.input.processor;
+
+import com.ksptooi.flr.entity.model.Model;
+import com.ksptooi.flr.input.annotation.Params;
+import com.ksptooi.flr.input.annotation.ProcessMapper;
+import com.ksptooi.flr.input.annotation.Processor;
+
+import java.util.Arrays;
+
+@Processor
+@ProcessMapper("cmd")
+public class TestProcessorSingle {
+
+    @ProcessMapper("cmd")
+    public Model testCommandProcessor(@Params("params")String[] p
+                                     ,@Params("model")Model model
+                                     ,@Params("name")String name){
+
+
+        model.addMessage("这是一个测试处理器222222");
+        model.addMessage("玩家命令名:"+name);
+        model.addMessage("参数:" + Arrays.toString(p));
+
+
+        System.out.println("这是一个测试处理器222222");
+
+        return model;
+    }
+
+
+}

9300a056eb40fce274de0263ecb4cd0375855743
90Neko <ksptooi@protonmail.ch>
2020-11-01 22:36:24 +0800

    Update

--- FastLoginR-PROCESSOR/src/main/java/com/ksptooi/flr/input/processor/AdminAccountProcessor.java
+++ FastLoginR-PROCESSOR/src/main/java/com/ksptooi/flr/input/processor/AdminAccountProcessor.java
@@ -1,15 +1,26 @@
 package com.ksptooi.flr.input.processor;
 
+import com.google.inject.Inject;
+import com.ksptooi.flr.entity.model.Model;
 import com.ksptooi.flr.input.annotation.ProcessMapper;
 import com.ksptooi.flr.input.annotation.Processor;
+import com.ksptooi.flr.proc.service.admin.AdminService;
 
 @Processor
-@ProcessMapper("")
+@ProcessMapper("flr")
 public class AdminAccountProcessor {
 
 
-
     //管理员重置玩家密码
+    @Inject
+    AdminService service = null;
+
+
+    @ProcessMapper(value = "changepassword")
+    public Model adminChangePlayerPassword(){
+        return null;
+    }
+
 
 
 

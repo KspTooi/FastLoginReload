a03418f39708bb7b748871a82d56863615e4c02b
Laotouy <48210313+Laotouy@users.noreply.github.com>
2020-11-01 22:30:58 +0800

    Update AdminAccountProcessor.java

--- FastLoginR-PROCESSOR/src/main/java/com/ksptooi/flr/input/processor/AdminAccountProcessor.java
+++ FastLoginR-PROCESSOR/src/main/java/com/ksptooi/flr/input/processor/AdminAccountProcessor.java
@@ -1,6 +1,8 @@
 package com.ksptooi.flr.input.processor;
 
+import com.google.inject.Inject;
 import com.ksptooi.flr.input.annotation.Processor;
+import com.ksptooi.flr.proc.service.admin.AdminService;
 
 @Processor
 public class AdminAccountProcessor {
@@ -8,7 +10,15 @@ public class AdminAccountProcessor {
 
 
     //管理员重置玩家密码
+    @Inject
+    AdminService service = null;
 
+
+
+//    @ProcessMapper(value = "changepassword")
+//    public Model adminChangePlayerPassword(){
+//
+//    }
 
 
 

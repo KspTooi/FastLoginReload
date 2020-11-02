c93835e0de3dc3a6c82b467d951692c4632f3d20
90Neko <ksptooi@protonmail.ch>
2020-11-02 16:48:54 +0800

    1.完善了Dispatch+Adapter+Resolver 2.整合以上三个组件 版本:1.4F.80

--- FastLoginR-PROCESSOR/src/main/java/com/ksptooi/flr/starter/FastLoginR.java
+++ FastLoginR-PROCESSOR/src/main/java/com/ksptooi/flr/starter/FastLoginR.java
@@ -15,7 +15,7 @@ import java.util.logging.Logger;
 
 public class FastLoginR extends JavaPlugin {
 
-    public static final String currentVersion = "1.4F.63";
+    public static final String currentVersion = "1.4F.80";
 
     public static final Injector injector= ProcModule.getInject();
 

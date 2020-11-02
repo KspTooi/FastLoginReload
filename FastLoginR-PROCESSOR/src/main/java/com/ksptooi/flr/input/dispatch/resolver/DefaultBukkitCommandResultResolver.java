ae98fdb851d3e191871cf45689372b8fc445aad2
90Neko <ksptooi@protonmail.ch>
2020-11-02 16:47:25 +0800

    解析器相关

--- FastLoginR-PROCESSOR/src/main/java/com/ksptooi/flr/input/dispatch/resolver/DefaultBukkitCommandResultResolver.java
+++ FastLoginR-PROCESSOR/src/main/java/com/ksptooi/flr/input/dispatch/resolver/DefaultBukkitCommandResultResolver.java
@@ -1,7 +1,25 @@
 package com.ksptooi.flr.input.dispatch.resolver;
 
-public class DefaultBukkitCommandResultResolver {
+import com.ksptooi.flr.entity.model.Model;
 
+import java.util.ArrayList;
+
+public class DefaultBukkitCommandResultResolver implements InputResultResolver{
+
+
+
+    @Override
+    public boolean ResolverModel(Model model) {
+
+        //MODEL解析 发送消息
+        ArrayList<String> message = model.getMessage();
+
+        for(String str:message){
+            model.getSender().sendMessage(str);
+        }
+
+        return true;
+    }
 
 
 }
--- FastLoginR-PROCESSOR/src/main/java/com/ksptooi/flr/input/dispatch/resolver/InputResultResolver.java
+++ FastLoginR-PROCESSOR/src/main/java/com/ksptooi/flr/input/dispatch/resolver/InputResultResolver.java
@@ -1,10 +1,13 @@
 package com.ksptooi.flr.input.dispatch.resolver;
 
+import com.ksptooi.flr.entity.model.Model;
+
 /**
  * Input返回解析器
  */
 public interface InputResultResolver {
 
-
+    //进行Model解析
+    boolean ResolverModel(Model model);
 
 }

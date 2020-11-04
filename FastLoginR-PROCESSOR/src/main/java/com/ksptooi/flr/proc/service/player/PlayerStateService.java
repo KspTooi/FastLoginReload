8963bacc4a6bc69e275dedd064e71e8f9c8bc684
90Neko <ksptooi@protonmail.ch>
2020-11-05 00:15:15 +0800

    实现玩家退出时 更新其状态

--- FastLoginR-PROCESSOR/src/main/java/com/ksptooi/flr/proc/service/player/PlayerStateService.java
+++ FastLoginR-PROCESSOR/src/main/java/com/ksptooi/flr/proc/service/player/PlayerStateService.java
@@ -1,6 +1,7 @@
 package com.ksptooi.flr.proc.service.player;
 
 
+import com.ksptooi.flr.proc.exception.StateUpdateException;
 import org.bukkit.entity.Player;
 
 /**
@@ -12,6 +13,6 @@ public interface PlayerStateService{
      * 玩家退出时改变玩家的数据库状态
      * @param player
      */
-    void playerQuitStateChange(Player player);
+    void playerQuitStateChange(Player player) throws StateUpdateException;
 
 }
--- FastLoginR-PROCESSOR/src/main/java/com/ksptooi/flr/proc/service/player/PlayerStateServiceBlock.java
+++ FastLoginR-PROCESSOR/src/main/java/com/ksptooi/flr/proc/service/player/PlayerStateServiceBlock.java
@@ -1,10 +1,60 @@
 package com.ksptooi.flr.proc.service.player;
 
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.ksptooi.flr.entity.player.FLRPlayer;
+import com.ksptooi.flr.entity.player.PlayerLocation;
+import com.ksptooi.flr.entity.status.ErrorStatus;
+import com.ksptooi.flr.entity.status.PlayerStatus;
+import com.ksptooi.flr.mapper.player.PlayerDetailMapper;
+import com.ksptooi.flr.mapper.player.PlayerLocationMapper;
+import com.ksptooi.flr.mapper.player.PlayerMapper;
+import com.ksptooi.flr.proc.exception.StateUpdateException;
+import com.ksptooi.flr.util.DateUtil;
+import com.ksptooi.flr.util.DtoUtil;
+import org.bukkit.entity.Player;
+import org.h2.message.DbException;
+import org.mybatis.guice.transactional.Transactional;
+
+@Singleton
+@Transactional
 public class PlayerStateServiceBlock implements PlayerStateService {
 
+    @Inject
+    PlayerMapper mapper = null;
 
+    @Inject
+    PlayerLocationMapper locMapper = null;
 
+    @Inject
+    PlayerDetailMapper detMapper = null;
 
+    /**
+     * 玩家退出时改变玩家的数据库状态
+     *
+     * @param player
+     */
+    @Override
+    public void playerQuitStateChange(Player player) throws StateUpdateException {
+
+        //获取玩家最新状态
+        FLRPlayer flrPlayer = DtoUtil.toPlayer(mapper.getPlayerByName(player.getName()), player);
+
+        //更新玩家状态
+        flrPlayer.setLoginStatus(PlayerStatus.LOGIN_FAILED.getCode());
+        flrPlayer.setLeaveDate(DateUtil.getCurTimeString());
+
+        //更新数据库
+        Integer updatePlayer = mapper.updatePlayer(flrPlayer);
+        Integer updateLocation = locMapper.updateLocation(flrPlayer.getPlayerLocation());
+        Integer updateDetail = detMapper.updateDetail(flrPlayer.getPlayerDetail());
+
+        //检查更新状态
+        if(updatePlayer<1 || updateLocation<1 || updateDetail<1){
+            throw new StateUpdateException(ErrorStatus.FATAL_DB);
+        }
+
+    }
 
 
 

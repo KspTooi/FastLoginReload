4bffd124d7b2df8b98a8f6498f38def937c6db85
90Neko <ksptooi@protonmail.ch>
2020-11-04 22:58:20 +0800

    新增State组件

---
+++ FastLoginR-PROCESSOR/src/main/java/com/ksptooi/flr/proc/service/player/PlayerStateService.java
@@ -0,0 +1,10 @@
+package com.ksptooi.flr.proc.service.player;
+
+
+/**
+ * 玩家状态改变服务
+ */
+public interface PlayerStateService{
+
+
+}
---
+++ FastLoginR-PROCESSOR/src/main/java/com/ksptooi/flr/proc/service/player/PlayerStateServiceBlock.java
@@ -0,0 +1,11 @@
+package com.ksptooi.flr.proc.service.player;
+
+public class PlayerStateServiceBlock implements PlayerStateService {
+
+
+
+
+
+
+
+}

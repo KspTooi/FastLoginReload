0b657b99afdd72615b7aa965130abda8fe4fcd7c
90Neko <ksptooi@protonmail.ch>
2020-11-05 00:14:59 +0800

    状态更新异常

---
+++ FastLoginR-PROCESSOR/src/main/java/com/ksptooi/flr/proc/exception/StateUpdateException.java
@@ -0,0 +1,18 @@
+package com.ksptooi.flr.proc.exception;
+
+import com.ksptooi.flr.entity.status.ErrorStatus;
+
+/**
+ * 玩家状态更新异常
+ */
+public class StateUpdateException extends ServiceException{
+
+    public StateUpdateException(ErrorStatus status) {
+        super(status);
+    }
+
+    public StateUpdateException(String msg) {
+        super(msg);
+    }
+
+}

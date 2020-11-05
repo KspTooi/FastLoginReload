970619a51d65fc2f37f692b7acc8d099d5afabd8
90Neko <ksptooi@protonmail.ch>
2020-11-06 01:09:12 +0800

    Comoon

---
+++ FastLoginR-UTIL/src/main/java/com/ksptooi/flr/util/DateUtil.java
@@ -0,0 +1,44 @@
+package com.ksptooi.flr.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 时间小工具
+ */
+public class DateUtil {
+
+
+    static SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+    /**
+     * 获取当前日期字符串
+     * @return 日期字符串
+     */
+    public static String getCurTimeString(){
+        return formatter.format(new Date());
+    }
+
+    /**
+     * 解析日期字符串
+     * @param timeString
+     * @return
+     */
+    public static Date parser(String timeString){
+
+        Date parse = null;
+
+        try{
+            formatter.parse(timeString);
+            return parse;
+        }catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+
+
+}

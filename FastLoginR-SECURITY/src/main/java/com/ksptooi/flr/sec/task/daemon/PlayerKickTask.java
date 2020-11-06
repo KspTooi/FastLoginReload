e2b7a14512fcc24d5ab73c834acb03d1bb46fcb2
90Neko <ksptooi@protonmail.ch>
2020-11-06 21:55:04 +0800

    更新TaskDaemon

--- FastLoginR-SECURITY/src/main/java/com/ksptooi/flr/sec/task/daemon/PlayerKickTask.java
+++ FastLoginR-SECURITY/src/main/java/com/ksptooi/flr/sec/task/daemon/PlayerKickTask.java
@@ -1,20 +1,16 @@
-package com.ksptooi.flr.sec.task.deamon;
+package com.ksptooi.flr.sec.task.daemon;
 
 
 import com.google.inject.Inject;
 import com.ksptooi.flr.sec.service.PlayerTaskQueueService;
-import org.bukkit.entity.Player;
-
-import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
- * 玩家消息队列
+ * 玩家踢出队列
  */
-public class PlayerMsgTask implements Runnable{
+public class PlayerKickTask implements Runnable{
 
     @Inject
     PlayerTaskQueueService taskQueueService = null;
-
 
     @Override
     public void run() {
@@ -32,20 +28,25 @@ public class PlayerMsgTask implements Runnable{
                 e.printStackTrace();
             }
 
+            System.out.println(second);
+
             //如果未超出间隔则继续循环
             if(second<interval){
                 continue;
             }
 
-            taskQueueService.refreshMessageQueue();
-            taskQueueService.sendMessage("§e[FastLogin]§b你需要登录后才能操作,使用/login 密码 来登录");
 
-            //超出间隔则向玩家发送信息
+
+            //超出间隔则检查玩家是否超时
+            taskQueueService.refreshKickQueue();
+            taskQueueService.kickPlayer();
+
+
             second=0;
         }
 
-    }
 
+    }
 
 
 
--- FastLoginR-SECURITY/src/main/java/com/ksptooi/flr/sec/task/daemon/PlayerMsgTask.java
+++ FastLoginR-SECURITY/src/main/java/com/ksptooi/flr/sec/task/daemon/PlayerMsgTask.java
@@ -1,19 +1,17 @@
-package com.ksptooi.flr.sec.task.deamon;
+package com.ksptooi.flr.sec.task.daemon;
 
 
 import com.google.inject.Inject;
 import com.ksptooi.flr.sec.service.PlayerTaskQueueService;
-import org.bukkit.entity.Player;
-
-import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
- * 玩家踢出队列
+ * 玩家消息队列
  */
-public class PlayerKickTask implements Runnable{
+public class PlayerMsgTask implements Runnable{
 
     @Inject
     PlayerTaskQueueService taskQueueService = null;
+
 
     @Override
     public void run() {
@@ -31,25 +29,20 @@ public class PlayerKickTask implements Runnable{
                 e.printStackTrace();
             }
 
-            System.out.println(second);
-
             //如果未超出间隔则继续循环
             if(second<interval){
                 continue;
             }
 
+            taskQueueService.refreshMessageQueue();
+            taskQueueService.sendMessage("§e[FastLogin]§b你需要登录后才能操作,使用/login 密码 来登录");
 
-
-            //超出间隔则检查玩家是否超时
-            taskQueueService.refreshKickQueue();
-            taskQueueService.kickPlayer();
-
-
+            //超出间隔则向玩家发送信息
             second=0;
         }
 
-
     }
+
 
 
 
